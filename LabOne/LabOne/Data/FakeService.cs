5fb479894d361a00f158f073464f34975c1977d0
Ross Byrne <ross.byrne@outlook.ie>
2016-09-21 11:43:27 +0100

    Added fields to persion

--- LabOne/LabOne/Data/FakeService.cs
+++ LabOne/LabOne/Data/FakeService.cs
@@ -11,6 +11,9 @@ namespace LabOne.Data
     {
         public String Name { get; set; }
         public int Age { get; set; }
+        public string Address { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Email { get; set; }
     }
 
     public class FakeService
--- LabOne/LabOne/ViewModels/PersonViewModel.cs
+++ LabOne/LabOne/ViewModels/PersonViewModel.cs
@@ -20,5 +20,23 @@ namespace ViewModels
             get { return This.Age; }
             set { SetProperty(This.Age, value, () => This.Age = value); }
         }
+
+        public string Address
+        {
+            get { return This.Address; }
+            set { SetProperty(This.Address, value, () => This.Address = value); }
+        }
+
+        public int PhoneNumber
+        {
+            get { return This.PhoneNumber; }
+            set { SetProperty(This.PhoneNumber, value, () => This.PhoneNumber = value); }
+        }
+
+        public string Email
+        {
+            get { return This.Email; }
+            set { SetProperty(This.Email, value, () => This.Email = value); }
+        }
     }
 }
