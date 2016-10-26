e07574b819120064b4fa34f9571871237bb7fd07
Ross Byrne <ross.byrne@outlook.ie>
2016-10-26 12:29:56 +0100

    Got authentication working

--- AppDevLab4_Windows_CS/AppDevLab4/MainPage.xaml.cs
+++ AppDevLab4_Windows_CS/AppDevLab4/MainPage.xaml.cs
@@ -14,6 +14,8 @@ using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
+using System.Linq;
+using Windows.Security.Credentials;
 
 #if OFFLINE_SYNC_ENABLED
 using Microsoft.WindowsAzure.MobileServices.SQLiteStore;  // offline sync
@@ -24,6 +26,9 @@ namespace AppDevLab4
 {
     public sealed partial class MainPage : Page
     {
+
+        private MobileServiceUser user;
+
         private MobileServiceCollection<TodoItem, TodoItem> items;
 #if OFFLINE_SYNC_ENABLED
         private IMobileServiceSyncTable<TodoItem> todoTable = App.MobileService.GetSyncTable<TodoItem>(); // offline sync
@@ -54,6 +59,73 @@ namespace AppDevLab4
 #if OFFLINE_SYNC_ENABLED
             await App.MobileService.SyncContext.PushAsync(); // offline sync
 #endif
+        }
+
+        private async System.Threading.Tasks.Task<bool> AuthenticateAsync()
+        {
+            string message;
+            bool success = false;
+
+            // This sample uses the Facebook provider.
+            var provider = MobileServiceAuthenticationProvider.WindowsAzureActiveDirectory;
+
+            // Use the PasswordVault to securely store and access credentials.
+            PasswordVault vault = new PasswordVault();
+            PasswordCredential credential = null;
+
+            try
+            {
+                // Try to get an existing credential from the vault.
+                credential = vault.FindAllByResource(provider.ToString()).FirstOrDefault();
+            }
+            catch (Exception)
+            {
+                // When there is no matching resource an error occurs, which we ignore.
+            }
+
+            if (credential != null)
+            {
+                // Create a user from the stored credentials.
+                user = new MobileServiceUser(credential.UserName);
+                credential.RetrievePassword();
+                user.MobileServiceAuthenticationToken = credential.Password;
+
+                // Set the user from the stored credentials.
+                App.MobileService.CurrentUser = user;
+
+                // Consider adding a check to determine if the token is 
+                // expired, as shown in this post: http://aka.ms/jww5vp.
+
+                success = true;
+                message = string.Format("Cached credentials for user - {0}", user.UserId);
+            }
+            else
+            {
+                try
+                {
+                    // Login with the identity provider.
+                    user = await App.MobileService
+                        .LoginAsync(provider);
+
+                    // Create and store the user credentials.
+                    credential = new PasswordCredential(provider.ToString(),
+                        user.UserId, user.MobileServiceAuthenticationToken);
+                    vault.Add(credential);
+
+                    success = true;
+                    message = string.Format("You are now logged in - {0}", user.UserId);
+                }
+                catch (MobileServiceInvalidOperationException)
+                {
+                    message = "You must log in. Login Required";
+                }
+            }
+
+            var dialog = new MessageDialog(message);
+            dialog.Commands.Add(new UICommand("OK"));
+            await dialog.ShowAsync();
+
+            return success;
         }
 
         private async Task RefreshTodoItems()
@@ -106,6 +178,19 @@ namespace AppDevLab4
             await RefreshTodoItems();
 
             ButtonRefresh.IsEnabled = true;
+        }
+
+        private async void ButtonLogin_Click(object sender, RoutedEventArgs e)
+        {
+            // Login the user and then load data from the mobile app.
+            if (await AuthenticateAsync())
+            {
+                // Switch the buttons and load items from the mobile app.
+                ButtonLogin.Visibility = Visibility.Collapsed;
+                ButtonSave.Visibility = Visibility.Visible;
+                //await InitLocalStoreAsync(); //offline sync support.
+                await RefreshTodoItems();
+            }
         }
 
         private async void ButtonSave_Click(object sender, RoutedEventArgs e)
